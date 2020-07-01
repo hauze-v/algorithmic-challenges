75108f679d506f6f69e9a8e28603032721dcbe03
Adam Hauze <hauze_v@protonmail.com>
2020-07-01 09:31:35 -0400

    Finished oppositeNumber challenge, which can be simply solved by returning -number

---
+++ oppositeNumber.js
@@ -0,0 +1,19 @@
+/*
+  Very simple, given a number, find its opposite
+*/
+
+// function opposite(number) {
+//   if (number > 0 ) {
+//     return (-1 * number);
+//   } else if (number < 0) {
+//     return Math.abs(number);
+//   } else {
+//     return number;
+//   }
+// }
+
+// Or more simply:
+
+function opposite(number) {
+  return -number;
+}
