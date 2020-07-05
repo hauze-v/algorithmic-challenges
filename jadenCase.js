0d68be45035827f07f0ebe7843f92ebd77b6d07c
Adam Hauze <hauze_v@protonmail.com>
2020-07-05 12:04:29 -0400

    Second way to solve jadenCase problem is to abstract the capitlziation to another method and use array.push instead of building the string manually.

--- jadenCase.js
+++ jadenCase.js
@@ -8,16 +8,34 @@
   Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 */
 
+// function toJadenCase(str) {
+//   let words = str.split(" ");
+//   let jadenTweet = "";
+
+//   for (let elem of words) {
+//     let word = elem.slice(0,1).toUpperCase();
+//     jadenTweet = jadenTweet + " " + word + elem.slice(1);
+//   }
+
+//   return jadenTweet;
+// }
+
+
+// Tenzin recommends abstracting the capitalization to another function
+function capitalize(word) {
+  return word[0].toUpperCase() + word.slice(1).toLowerCase();
+}
+
+
 function toJadenCase(str) {
   let words = str.split(" ");
-  let jadenTweet = "";
+  let result = [];
 
-  for (let elem of words) {
-    let word = elem.slice(0,1).toUpperCase();
-    jadenTweet = jadenTweet + " " + word + elem.slice(1);
+  for (let word of words) {
+    result.push(capitalize(word))
   }
-
-  return jadenTweet;
+  return result.join(" ");
 }
+
 
 console.log(toJadenCase("how can mirrors be real if our eyes aren't real"))
