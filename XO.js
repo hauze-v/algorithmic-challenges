5418b0a69bf425cb2b98ed9e9708f75a0a7a3297
Adam Hauze <hauze_v@protonmail.com>
2020-07-09 08:52:45 -0400

    Solved Exes and Ohs fairly easily by splitting str into an array and using a for of loop to count the Ohs and Exes

---
+++ XO.js
@@ -0,0 +1,36 @@
+/*
+  Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+  Examples input/output:
+    XO("ooxx") => true
+    XO("xooxx") => false
+    XO("ooxXm") => true
+    XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+    XO("zzoo") => false
+*/
+
+function XO(str) {
+  // Split string into an array of characters
+  let characters = str.split("");
+  let x = 0;
+  let o = 0;
+  
+  for (let char of characters) {
+    if (char == "o" || char == "O") {
+      o++;
+    }
+
+    if (char == "x" || char == "X") {
+      x++;
+    }
+  }
+
+  if (x === o) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(XO("ooxx")) // true
+console.log(XO("xooxx")) // false
