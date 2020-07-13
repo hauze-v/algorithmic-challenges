f2282c74c7c9b757f25d42c008acbc15383d47ae
hauze-v <hauze_v@protonmail.com>
2020-07-13 09:49:22 -0400

    Another way to solve this problem using math.min and Math.max and passing in the numbers array

--- highest_and_lowest.js
+++ highest_and_lowest.js
@@ -26,3 +26,11 @@ function highAndLow(str) {
 console.log(highAndLow("1 2 3 4 5"));  // return "5 1"
 console.log(highAndLow("1 2 -3 4 5")); // return "5 -3"
 console.log(highAndLow("1 9 3 4 -5")); // return "9 -5"
+
+/* Another possible way to do this would be using Math.max and Math.min: */
+
+// function highAndLow(numbers){
+//   numbers = numbers.split(' ');
+  
+//   return Math.max(...numbers) + ' ' + Math.min(...numbers);
+// }
