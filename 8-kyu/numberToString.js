a335c0955f38fdbb90dae5b549a16e8414c9632b
hauze-v <hauze_v@protonmail.com>
2020-07-20 13:15:54 -0400

    Completed numberToString problem

---
+++ 8-kyu/numberToString.js
@@ -0,0 +1,16 @@
+/*
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+Examples:
+
+numberToString(123); // returns '123';`   
+numberToString(999); // returns '999';`
+
+*/
+
+function numberToString(n) {
+  return n.toString()
+}
+
+console.log(numberToString(123))
