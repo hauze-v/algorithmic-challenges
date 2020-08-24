f43821d92f7c2fc88aae34e4fbb9afe8f0d62a90
hauze-v <hauze_v@protonmail.com>
2020-08-24 11:08:58 -0400

    Finished yourOrderPlease codewar problem. Needed to use an additional counter with my for loop and leverage the indexOf method

--- 6-kyu/yourOrderPlease.js
+++ 6-kyu/yourOrderPlease.js
@@ -12,31 +12,59 @@
   ""  -->  "" 
 */
 
-function order(str) {
-  if (!str) {
-    return '';
-  }
+// function order(str) {
+//   if (!str) {
+//     return '';
+//   }
   
    
-}
+// }
 
-/* Converts string to array of words, pulls number from each word and builds an array of numbers */
-function getNumbers(str) {
-  /* Define an obj to hold the words and their order in the string */
-  let obj = {};
+// /* Converts string to array of words, pulls number from each word and builds an array of numbers */
+// function getNumbers(str) {
+//   /* Define an obj to hold the words and their order in the string */
+//   let obj = {};
  
-  /* Split the string into an array of words */ 
-  let words = str.split(" ");
+//   /* Split the string into an array of words */ 
+//   let words = str.split(" ");
   
-  /* Loop through the array of words and add each word with the order number as a key-value pair in obj */
-  for (let i = 0; i < words.length; i++) {
+//   /* Loop through the array of words and add each word with the order number as a key-value pair in obj */
+//   for (let i = 0; i < words.length; i++) {
 
-    obj.words[i] = 'ham';
+//     obj.words[i] = 'ham';
+//   }
+
+//   console.log(obj)
+// }
+
+// // console.log(order())
+// // console.log(order('is2 Thi1s T4est 3a')) // Thi1s is2 3a T4est
+// console.log(getNumbers('is2 Thi1s T4est 3a'))
+
+function order(words) {
+  /* Return empty string if given empty string */
+  if(words === "") {
+    return "";
   }
 
-  console.log(obj)
+  /* Define result and split the given string into an array of words */
+  let result = [];
+  words = words.split(" ");
+  
+  /* Define a counter variable used to determine the indexOf each word: starting with 1 */
+  let j = 1;
+
+  /* Loop through the array of words */
+  for (let i = 0; i < words.length; i++) {
+    /* If the indexOf j is greater than -1 (it exists) push that word to the array because it's next in line. Then incredment j */
+    if(words[i].indexOf(j) > -1) {
+      result.push(words[i]);
+      j++;
+      i = -1; /* We then have to restart the loop, to scan through for the next word */
+    }
+  }
+
+  return result.join(" ");
 }
 
-// console.log(order())
-// console.log(order('is2 Thi1s T4est 3a')) // Thi1s is2 3a T4est
-console.log(getNumbers('is2 Thi1s T4est 3a'))
+console.log(order('is2 Thi1s T4est 3a'))
