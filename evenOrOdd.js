91c4882c7014c8fa0333860a4a559ce609ac06ed
Adam Hauze <hauze_v@protonmail.com>
2020-08-29 08:15:08 -0400

    Solved even_or_odd codewars problem

---
+++ evenOrOdd.js
@@ -0,0 +1,15 @@
+/*
+Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+*/
+
+function even_or_odd(number) {
+  if (number % 2 === 0) {
+    // number is even
+    return "Even"
+  } else {
+    return "Odd"
+  }
+}
+
+console.log(even_or_odd(3))
