12bcb63795617e666fcf62ab35bdf81a51cb906a
Adam Hauze <hauze_v@protonmail.com>
2020-09-05 08:39:17 -0400

    Finished remove first and last char

---
+++ stream/oppositeNumber.js
@@ -0,0 +1,17 @@
+/*
+  Very simple, given a number, find its opposite.
+
+  Examples:
+
+  1: -1
+  14: -14
+  -34: 34
+*/
+
+function opposite(number) {
+  return (number * (-1));
+}
+
+console.log(opposite(1)) // -1
+console.log(opposite(14)) // -14
+console.log(opposite(-34)) // 34
