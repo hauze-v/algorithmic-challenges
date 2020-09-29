ece1df61ea033a0bed4c9a96d7857eebec6f7572
hauze-v <hauze_v@protonmail.com>
2020-09-29 14:48:29 -0400

    Completed Regular Ball Super Ball problem

---
+++ codewars/8-kyu/oop/regularBallSuperBall.js
@@ -0,0 +1,30 @@
+/*
+Create a class Ball.
+
+Ball objects should accept one argument for "ball type" when instantiated.
+
+If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+ball1 = new Ball();
+ball2 = new Ball("super");
+
+ball1.ballType --> 'regular'
+ball2.ballType --> 'super'
+*/
+
+class Ball {
+  constructor(ballType) {
+    if (ballType) {
+      this.ballType = ballType;
+    } else {
+      this.ballType = "regular";
+    }
+
+  }
+}
+
+const ball1 = new Ball();
+const ball2 = new Ball("super");
+
+console.log(ball1.ballType);
+console.log(ball2.ballType);
