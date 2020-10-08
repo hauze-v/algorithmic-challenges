ca6195d4485978501e98df55929bac408696c88d
hauze-v <hauze_v@protonmail.com>
2020-10-08 10:50:26 -0400

    Finished defanging IP address problem using for loop.

---
+++ leetcode/defangingIPAddr.js
@@ -0,0 +1,37 @@
+/* Given a valid (IPv4) IP address, return a defanged version of that IP address.
+A defanged IP address replaces every period "." with "[.]".
+
+Example 1:
+Input: address = "1.1.1.1"
+Output: "1[.]1[.]1[.]1"
+
+Example 2:
+Input: address = "255.100.50.0"
+Output: "255[.]100[.]50[.]0" */
+
+// function defangIPaddr(input) {
+//   let addrArray = input.split("");
+
+//   for (let i = 0; i < addrArray.length; i++) {
+//     if (addrArray[i] === ".") {
+//       addrArray[i] = "[.]";
+//     }
+//   }
+
+//   return addrArray.join("");
+// }
+
+function defangIPaddr(input) {
+  let addrArray = input.split("");
+
+  for (let i = 0; i < addrArray.length; i++) {
+    if (addrArray[i] === ".") {
+      addrArray[i] = "[.]";
+    }
+  }
+
+  return addrArray.join("");
+}
+
+console.log(defangIPaddr("1.1.1.1")); // "1[.]1[.]1[.]1"
+console.log(defangIPaddr("255.100.50.0")); // "255[.]100[.]50[.]0"
